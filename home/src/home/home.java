cf80d14760533b74d56ca47fba41230481758071
KimJaeHyeon <kjh94kjh94@naver.com>
2016-09-23 13:16:16 +0900

    12

--- home/src/home/home.java
+++ home/src/home/home.java
@@ -5,7 +5,7 @@ public class home {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                   
-		System.out.print("hello ");
+		System.out.print("hello big bro~");
 	}
 
 }
