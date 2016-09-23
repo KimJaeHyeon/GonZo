fe5504f17e6783120b9259e77287a553e68e9bdb
KimJaeHyeon <kjh94kjh94@naver.com>
2016-09-23 13:15:02 +0900

    11

--- home/src/home/home.java
+++ home/src/home/home.java
@@ -1,6 +1,11 @@
 package home;
 
 public class home {
-	111
-		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                  
+		System.out.print("hello ");
+	}
+
 }
